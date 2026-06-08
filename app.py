f6037c0d8dbdc50c3985978e488b7b493a523156
tanushreeshahapurmath03-lang <tanushreeshahapurmath03@gmail.com>
2026-06-08 08:52:10 +0000

    added python programs

---
+++ app.py
@@ -0,0 +1,14 @@
+print("namaskar")
+boy_name = input("boy name: ")
+boy_age =int(input("Boy Age: "))
+girl_name = input("girl_name: ")
+
+girl_age = int(input("Girl_age: "))
+
+age_diff = abs(boy_age - girl_age)
+
+print(boy_name)
+print(girl_name)
+
+print(boy_name + " loves " + girl_name + ".Age difference is " + str(age_diff))
+
